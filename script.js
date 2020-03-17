2ec3f6a14c0519c392c2ae0f40ad8d7ad6b2a0f5
eugorgomes <51417124+eugorgomes@users.noreply.github.com>
2020-03-17 01:29:25 -0300

    Site de instalações

---
+++ script.js
@@ -0,0 +1,13 @@
+if ($(window).width() <= 991) {
+   
+$("#textoSM").css("top", "1rem");
+$("#contatos").css("margin-top", "3rem");
+
+}
+
+if ($(window).width() >991) {
+   
+$("#textoSM").css("top", "5.7rem");
+$("#contatos").css("margin-top", "3rem");
+
+}
